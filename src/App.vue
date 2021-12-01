990e53858343361be7145b4f8af2145fdb4f2e00
Ileenfdz <ileenfdz@gmail.com>
2021-12-01 14:12:57 +0100

    modified styles

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,39 @@
 <template>
-  <v-app id="inspire">
+  <v-app id="inspire" font-family="Rasa">
     <v-header>
-      <div class="color1">
-        <h1 align-center justify-center>
-          Hello I am center to vertically using "align-center".
-        </h1>
+      <div class="logotype">
+        <h1>Logotype</h1>
       </div>
     </v-header>
     <v-main>
-      <v-subheader class="list">
-        <h2>Hello World</h2>
+      <v-subheader class="views">
+        <div>
+          <v-btn
+            class="btn-list"
+            color="rgba(144, 83, 83, 1)"
+            outlined
+            rounded
+            text
+            >Lista</v-btn
+          >
+        </div>
+        <div>
+          <v-btn
+            class="btn-favourites"
+            color="rgba(144, 83, 83, 1)"
+            outlined
+            rounded
+            text
+            >Favoritos</v-btn
+          >
+        </div>
       </v-subheader>
       <v-container>
         <v-row>
           <v-col v-for="n in 9" :key="n" cols="4">
             <v-card
               class="mx-auto rounded-xl"
-              max-width="357"
+              min-width="357"
               height="180"
               color="#FFC086"
             >
@@ -35,36 +52,40 @@
                 </v-list-item-content>
               </v-list-item>
               <v-card-actions>
-                <v-btn outlined rounded text> Editar </v-btn>
+                <v-btn
+                  class="btn-edit"
+                  color="rgba(144, 83, 83, 1)"
+                  outlined
+                  rounded
+                  text
+                >
+                  Editar
+                </v-btn>
               </v-card-actions>
             </v-card>
           </v-col>
         </v-row>
       </v-container>
     </v-main>
-    <v-footer color="primary lighten-1" padless>
+    <v-footer color="rgba(255, 224, 144, 1)" padless>
       <v-row justify="center" no-gutters>
         <v-btn
           v-for="link in links"
           :key="link"
-          color="white"
+          color="rgba(144, 83, 83, 1)"
           text
           rounded
-          class="my-2"
+          class="my-2 links"
         >
           {{ link }}
         </v-btn>
-        <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
-          <strong class="">Copyright</strong>
+        <v-col class="text-center" cols="12">
+          <strong class="copyright">Copyright</strong>
         </v-col>
       </v-row>
     </v-footer>
   </v-app>
 </template>
-
-<style>
-@import url("https://fonts.googleapis.com/css2?family=Rasa:wght@300&display=swap");
-</style>
 
 <script>
 export default {
@@ -77,17 +98,47 @@ export default {
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Rasa:wght@300&display=swap");
+v-app {
+  font-family: Rasa, "Rasa";
+}
 v-header {
-  background-color: red;
-  .color1 {
-    background-color: blue;
+  background-color: rgba(255, 224, 144, 1);
+  height: 100px;
+  .logotype {
     justify-content: center;
     display: flex;
   }
 }
-.list {
-  background-color: green;
+.views {
+  margin-top: 25px;
+  margin-bottom: 25px;
   justify-content: center;
   display: flex;
+  .btn-list {
+    width: 127px;
+    height: 37px;
+    margin-right: 20px;
+    background: #bafefa;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+  .btn-favourites {
+    background: #bafefa;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  }
+}
+.btn-edit {
+  background: #bafefa;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.links {
+  widows: 37px;
+  height: 112px;
+  margin-right: 10px;
+  background: #bafefa;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+.copyright {
+  color: rgba(144, 83, 83, 1);
 }
 </style>
