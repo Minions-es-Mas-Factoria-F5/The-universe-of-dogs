66d908f16c415c4e65ddc370a9a574d5f49809cc
imoreno73 <92292636+imoreno73@users.noreply.github.com>
2021-12-07 15:35:27 +0100

    added function gennewimage

---
+++ src/views/Newimage.js
@@ -0,0 +1,24 @@
+function genNewImage() {
+  "https://dog.ceo/api/breeds/image/random" // pendiente fetch
+    .then((body) => {
+      let url = body.data.message;
+      let image = document.getElementById("dogImg");
+      let dogText = document.getElementById("dogText");
+
+      let urlCutter = url.split("/")[4].split("-");
+      let first =
+        typeof urlCutter[1] == "undefined"
+          ? ""
+          : urlCutter[1].charAt(0).toUpperCase() +
+            urlCutter[1].split("").slice(1, urlCutter[1].length).join("");
+      let second =
+        urlCutter[0].charAt(0).toUpperCase() +
+        urlCutter[0].split("").slice(1, urlCutter[0].length).join("");
+      image.src = url; // set the src of the image object
+      dogText.innerHTML = "This is a: " + first + " " + second; //Set the dog breed name in the paragraph
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
+}
+genNewImage();
